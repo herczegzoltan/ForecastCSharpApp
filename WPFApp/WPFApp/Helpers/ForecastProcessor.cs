7218504bb61b59998bdf5ca9247da1b087389c29
Zoltan Herczeg <herczeg.zoltan92@gmail.com>
2019-04-24 12:12:07 +0200

    Add ForecastProcessor class to load the forecast information

--- WPFApp/WPFApp/Helpers/ForecastProcessor.cs
+++ WPFApp/WPFApp/Helpers/ForecastProcessor.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using WPFApp.Models;
 
 namespace WPFApp.Helpers
 {
     public class ForecastProcessor
     {
-
+        public static async Task<RootObject> LoadForecastInformation(string latitudeAndLongitude, string language)
+        {
+            string url = $"https://api.darksky.net/forecast/2fcabea5d1b6cd5c9aee4d570d4b1ae0/{ latitudeAndLongitude}?lang={language}&units=auto";
+            using (HttpResponseMessage responseMessage = await DarkSkyAPI.DarkSkyApiClient.GetAsync(url))
+            {
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    RootObject result = await responseMessage.Content.ReadAsAsync<RootObject>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(responseMessage.ReasonPhrase);
+                }
+            }
+        }
     }
 }
