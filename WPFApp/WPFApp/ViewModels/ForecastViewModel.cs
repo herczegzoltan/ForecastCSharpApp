96cc7287f892c8ce29149fa188c7fbc63c602291
Zoltan Herczeg <herczeg.zoltan92@gmail.com>
2019-04-24 12:29:16 +0200

    Add TextBlock,Button to Window

--- WPFApp/WPFApp/ViewModels/ForecastViewModel.cs
+++ WPFApp/WPFApp/ViewModels/ForecastViewModel.cs
@@ -10,5 +10,18 @@ namespace WPFApp.ViewModels
 {
     public class ForecastViewModel : Screen
     {
+        public ForecastViewModel()
+        {
+            DarkSkyAPI.InitializeClient();
+        }
+
+        public async void GetForecastInformation()
+        {
+            var forecastInfo = await ForecastProcessor.LoadForecastInformation("47.49801,19.03991", "hu");
+
+            //forecastInfo.currently.summary;
+        }
+
+
     }
 }
